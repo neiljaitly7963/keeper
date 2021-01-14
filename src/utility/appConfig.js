d82544d7297729dc42ded8dce7ec30039fa333f3
Neil <neiljaitly7963@gmail.com>
2021-01-14 17:47:12 +0530

    config file changed

--- src/utility/appConfig.js
+++ src/utility/appConfig.js
@@ -1,36 +1,38 @@
-import { get, cloneDeep } from 'lodash'
+import { get, cloneDeep } from 'lodash';
 
 import AppConfig from '../mock/data/AppConfig.json';
 
-if (process.env.NODE_ENV === 'development') {
-  window.AppConfig = AppConfig;
-}
-if (process.env.REACT_APP_USE_MOCK) {
-  window.AppConfig.api.baseUrl = '/api/';
-}
+// if (process.env.NODE_ENV === 'development') {
+//   window.AppConfig = AppConfig;
+// }
+// if (process.env.REACT_APP_USE_MOCK) {
+//   window.AppConfig.api.baseUrl = '/api/';
+// }
+
+window.AppConfig = AppConfig;
+s;
 export default class {
-  static config = cloneDeep(window.AppConfig)
+  static config = cloneDeep(window.AppConfig);
 
-  static get (path, fallback = null) {
-    return get(this.config, path, fallback)
+  static get(path, fallback = null) {
+    return get(this.config, path, fallback);
   }
 
-  static getApiBaseUrl () {
-    return this.get('api.baseUrl')
+  static getApiBaseUrl() {
+    return this.get('api.baseUrl');
   }
 
-  static getResourceUrl (path) {
+  static getResourceUrl(path) {
     // If there is no path, return an empty string
-    if ( !path ) return '';
+    if (!path) return '';
 
     // If the path already contains the resource url, we return the path because it is already a full url
-    if(path.startsWith('download') || path.startsWith('/download')) {
-      const baseUrl = this.get('backend.resourceUrl')
+    if (path.startsWith('download') || path.startsWith('/download')) {
+      const baseUrl = this.get('backend.resourceUrl');
       // return base + path
-      return `${baseUrl}/${path}`
+      return `${baseUrl}/${path}`;
     }
 
-    return path
+    return path;
   }
-
 }
